a79c7e77a13114bb21dc89ba35c112b9490c8931
Pumin Wangprasit <savee2011@hotmail.com>
2024-06-14 11:49:06 +0700

    Class Activity - W11 EX3

---
+++ Class_Activity/Activity_10/ComputeAvg.java
@@ -0,0 +1,27 @@
+package Class_Activity.Activity_10;
+
+import java.util.Scanner;
+
+public class ComputeAvg {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter your test scores (5 numbers): ");
+
+		double[] scores = new double[5]; // Array to store the test scores
+
+		for (int i = 0; i < scores.length; i++) { // Loop to get the test scores
+			scores[i] = scanner.nextDouble(); // Get the test score
+		}
+
+		double average = 0;
+		for (double score : scores) {
+			average = average + score; // Sum of all the test scores
+		}
+
+		average = average / scores.length; // Average of the test scores
+
+		System.out.println("The average of your test scores is: " + average);
+		scanner.close();
+	}
+}
