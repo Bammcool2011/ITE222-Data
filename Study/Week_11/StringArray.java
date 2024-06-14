e6ec33325a90e6e1e73cb7af44e56919ebd77f08
Pumin Wangprasit <savee2011@hotmail.com>
2024-06-14 11:06:34 +0700

    Arrays - String

---
+++ Study/Week_11/StringArray.java
@@ -0,0 +1,12 @@
+package Study.Week_11;
+
+public class StringArray {
+
+	public static void main(String args[]) {
+		String names[] = { "Tom", "David", "Mike" };
+
+		for (int idx = 0; idx < names.length; idx++) {
+			System.out.println(names[idx]);
+		}
+	}
+}
